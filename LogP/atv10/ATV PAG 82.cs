8e835e10e6fe68488230684d91a0d1b6dfed4b0c
Pedro Carvalho Mattar <124889462+Pedro-C-Mattar@users.noreply.github.com>
2025-10-01 09:46:55 -0300

    Add files via upload

---
+++ LogP/atv10/ATV PAG 82.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Página_82
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnQ1_Click(object sender, EventArgs e)
+        {
+            labelQ1.Text = string.Empty;
+            int[,] mat = new int[3, 4];
+            for (int linha = 0; linha < mat.GetLength(0); linha++)
+            {
+                for (int coluna = 0; coluna < mat.GetLength(1); coluna++)
+                {
+                    mat[linha, coluna] = linha * coluna;
+                }
+            }
+            for (int linha = 0; linha < mat.GetLength(0); linha++)
+            {
+                for (int coluna = 0; coluna < mat.GetLength(1); coluna++)
+                {
+                    lblQ1.Text += mat[linha, coluna] + " ";
+                }
+                labelQ1.Text += "\n";
+            }
+        }
+
+        private void BtnQ2_Click(object sender, EventArgs e)
+        {
+            labelQ2.Text = string.Empty;
+            int[,] mat = new int[4, 4];
+            for (int linha = 0; linha < mat.GetLength(0); linha++)
+            {
+                for (int coluna = 0; coluna < mat.GetLength(1); coluna++)
+                {
+                    mat[linha, coluna] = coluna;
+                }
+            }
+            for (int linha = 0; linha < mat.GetLength(0); linha++)
+            {
+                for (int coluna = 0; coluna < mat.GetLength(1); coluna++)
+                {
+                    labelQ2.Text += mat[linha, coluna] + " ";
+                }
+                labelQ2.Text += "\n";
+            }
+        }
+
+        private void BtnQ3_Click(object sender, EventArgs e)
+        {
+            labelQ3.Text = string.Empty;
+            Random random = new Random();
+            int[,] mat = new int[5, 3];
+            int soma = 0;
+            for (int linha = 0; linha < mat.GetLength(0); linha++)
+            {
+                for (int coluna = 0; coluna < mat.GetLength(1); coluna++)
+                {
+                    mat[linha, coluna] = random.Next(0, 9 + 1);
+                }
+            }
+            for (int linha = 0; linha < mat.GetLength(0); linha++)
+            {
+                for (int coluna = 0; coluna < mat.GetLength(1); coluna++)
+                {
+                    labelQ3.Text += mat[linha, coluna] + " ";
+                    soma += mat[linha, coluna];
+                }
+                labelQ3.Text += "\n";
+            }
+            float Media = (float)soma / mat.Length;
+            labelMedia.Text = "Média: " + Media;
+        }
+
+        private void BtnQ4_Click(object sender, EventArgs e)
+        {
+            labelQ5.Text = string.Empty;
+            int[,] mat = new int[3, 3];
+            for (int linha = 0; linha < mat.GetLength(0); linha++)
+            {
+                for (int coluna = 0; coluna < mat.GetLength(1); coluna++)
+                {
+                    mat[linha, coluna] = 0;
+                }
+            }
+            for (int linha = 0; linha < mat.GetLength(0); linha++)
+            {
+                for (int coluna = 0; coluna < mat.GetLength(1); coluna++)
+                {
+                    if (linha == coluna)
+                        labelQ5.Text += mat[linha, coluna] + " ";
+                    else
+                        labelQ5.Text += "  ";
+                }
+                labelQ5.Text += "\n";
+            }
+        }
+
+        private void BtnQ6_Click(object sender, EventArgs e)
+        {
+            labelQ6.Text = string.Empty;
+            labelSoma.Text = "Soma Final: ";
+            labelVetor.Text = "Vetor: ";
+            Random random = new Random();
+            int[,] mat = new int[4, 6];
+            int[] SomaColuna = new int[6];
+            int SomaFinal = 0;
+            for (int linha = 0; linha < mat.GetLength(0); linha++)
+            {
+                for (int coluna = 0; coluna < mat.GetLength(1); coluna++)
+                {
+                    mat[linha, coluna] = random.Next(0, 9 + 1);
+                }
+            }
+            for (int linha = 0; linha < mat.GetLength(0); linha++)
+            {
+                for (int coluna = 0; coluna < mat.GetLength(1); coluna++)
+                {
+                    labelQ6.Text += mat[linha, coluna] + " ";
+                    SomaColuna[coluna] += mat[linha, coluna];
+                }
+                labelQ6.Text += "\n";
+            }
+            for (int i = 0; i < SomaColuna.Length; i++)
+            {
+                labelVetor.Text += SomaColuna[i] + " ";
+                SomaFinal += SomaColuna[i];
+            }
+            labelSoma.Text += SomaFinal;
+        }
+
+        private void BtnQ7_Click(object sender, EventArgs e)
+        {
+            labelQ7a.Text = string.Empty;
+            Random random = new Random();
+            int[,] mat1 = new int[3, 5];
+            int[,] mat2 = new int[3, 5];
+            int[,] mat3 = new int[3, 5];
+            for (int linha = 0; linha < mat1.GetLength(0); linha++)
+            {
+                for (int coluna = 0; coluna < mat1.GetLength(1); coluna++)
+                {
+                    mat1[linha, coluna] = random.Next(10, 49 + 1);
+                    labelQ7a.Text += mat1[linha, coluna] + " ";
+                }
+                labelQ7a.Text += "\n";
+            }
+            for (int linha = 0; linha < mat2.GetLength(0); linha++)
+            {
+                for (int coluna = 0; coluna < mat2.GetLength(1); coluna++)
+                {
+                    mat2[linha, coluna] = random.Next(10, 49 + 1);
+                    labelQ7b.Text += mat2[linha, coluna] + " ";
+                }
+                labelQ7b.Text += "\n";
+            }
+            for (int linha = 0; linha < mat3.GetLength(0); linha++)
+            {
+                for (int coluna = 0; coluna < mat3.GetLength(1); coluna++)
+                {
+                    mat3[linha, coluna] = (mat1[linha, coluna]) + (mat2[linha, coluna]);
+                    labelQ7c.Text += mat3[linha, coluna] + " ";
+                }
+                labelQ7c.Text += "\n";
+            }
+        }
+
+        private void BtnQ9_Click(object sender, EventArgs e)
+        {
+            labelQ9.Text = string.Empty;
+            labelQ9Transp.Text = string.Empty;
+            int[,] mat = new int[3, 5];
+            int[,] matT = new int[mat.GetLength(1), mat.GetLength(0)];
+            Random random = new Random();
+            for (int linha = 0; linha < mat.GetLength(0); linha++)
+            {
+                for (int coluna = 0; coluna < mat.GetLength(1); coluna++)
+                {
+                    mat[linha, coluna] = random.Next(1, 9 + 1);
+                }
+            }
+            for (int linha = 0; linha < mat.GetLength(0); linha++)
+            {
+                for (int coluna = 0; coluna < mat.GetLength(1); coluna++)
+                {
+                    labelQ9.Text += mat[linha, coluna] + " ";
+                }
+                labelQ9.Text += "\n";
+            }
+            for (int linha = 0; linha < matT.GetLength(0); linha++)
+            {
+                for (int coluna = 0; coluna < matT.GetLength(1); coluna++)
+                {
+                    matT[linha, coluna] = mat[coluna, linha];
+                    labelQ9Transp.Text += matT[linha, coluna] + " ";
+                }
+                labelQ9Transp.Text += "\n";
+            }
+        }
+    }
+}
---
+++ LogP/atv11/atv pag 57 n 5.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atv_pag_57_n_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "";
+            string sexo = "";
+            int idade = 0;
+            int time = 0;
+            int contgalo = 0;
+            int contpaulo = 0;
+            int contmengo = 0;
+            int contrinthias = 0;
+            int contotros = 0;
+            int contador = 0;
+            double galocent = 0;
+            double otrocent = 0;
+            string maior = "";
+            int femigalo = 0;
+            int idamengo = 0;
+            double mengocent = 0;
+            int contdade = 0;
+            double mediadade = 0;
+
+            while(time != -1)
+            {
+                Console.WriteLine("Digite Nome: ");
+                nome = Console.ReadLine();
+                Console.WriteLine("Digite Sexo (feminino ou masculino): ");
+                sexo = Console.ReadLine();
+                Console.WriteLine("Digite Idade: ");
+                idade = int.Parse(Console.ReadLine());
+                Console.WriteLine("Digite Time (1 - Galo, 2 - Sao Paulo, 3 - Flamengo, 4 - Corinthias, 5 - Outros) ou (-1 para sair): ");
+                time = int.Parse(Console.ReadLine());
+
+                if(time == 1)
+                {
+                    contgalo++;
+                }
+                if(time == 2)
+                {
+                    contpaulo++;
+                }
+                if(time == 3)
+                {
+                    contmengo++;
+                }
+                if(time == 4)
+                {
+                    contrinthias++;
+                }
+                if(time == 5)
+                {
+                    contotros++;
+                }
+                if(contgalo > contpaulo)
+                {
+                    maior = "Atletico-MG";
+                }
+                if(contpaulo > contgalo)
+                {
+                    maior = "Sao Paulo";
+                }
+                if(sexo == "feminino" && time == 1)
+                {
+                    femigalo++;
+                }
+                if(idade <= 30 && time == 3)
+                {
+                    idamengo++;
+                }
+                if(time == 4)
+                {
+                    contdade = contdade + idade;
+                }
+
+                contador++;
+            }
+            contador = contador - 1;
+            galocent = ((double)contgalo / contador) * 100;
+            otrocent = ((double)contotros / contador) * 100;
+            mengocent = ((double)idamengo / contador) * 100;
+            mediadade = contdade / contador;
+
+            Console.WriteLine("Torcem para o Galo: " + contgalo + " ou " + galocent + " porcento. ");
+            Console.WriteLine("Torcem para outros times: " + contotros + " ou " + otrocent + " porcento. ");
+            Console.WriteLine("O time preferido é: " + maior);
+            Console.WriteLine("Quantidade de atleticanas: " + femigalo);
+            Console.WriteLine("Porcentagem de flamenguistas ate 30 anos: " + mengocent + " porcento. ");
+            Console.WriteLine("Media das idades dos corinthianos(a) é: " + mediadade);
+            Console.ReadKey();
+        }
+    }
+}
